497ed60bd0356456c5208842a47c5f950e440183
Jeffrey Wu <jwu.mathscience@gmail.com>
2017-01-13 23:35:15 -0500

    Renamed variables to be more intuitive

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -9,22 +9,23 @@ module Associatable
     define_method(name) do
       through_options = self.class.assoc_options[through_name]
       source_options = through_options.model_class.assoc_options[source_name]
-      foreign_key = self.send(through_options.foreign_key)
+      through_foreign = self.send(through_options.foreign_key)
 
       source_table = source_options.model_class.table_name
       through_table = through_options.model_class.table_name
-      through_foreign = "#{through_table}.#{through_options.foreign_key}"
+      through_primary = "#{through_table}.#{through_options.primary_key}"
+      source_foreign = "#{through_table}.#{source_options.foreign_key}"
       source_primary = "#{source_table}.#{source_options.primary_key}"
       source_class = source_options.model_class
 
-      query = DBConnection.execute(<<-SQL, foreign_key)
+      query = DBConnection.execute(<<-SQL, through_foreign)
         SELECT
           #{source_table}.*
         FROM
           #{source_table}
         JOIN
           #{through_table}
-          ON #{through_foreign} = #{source_primary}
+          ON #{source_foreign} = #{source_primary}
         WHERE
           #{through_primary} = ?
       SQL
